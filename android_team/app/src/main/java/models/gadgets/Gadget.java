9f9611eb5845a8c070ae4f7b7b9a75aa6830d894
Tobias Andersson <tobiasgustaverik@icloud.com>
2020-10-26 11:23:14 +0100

    Tobzki lade till adapter klassen och diverse metoder

--- android_team/app/src/main/java/models/gadgets/Gadget.java
+++ android_team/app/src/main/java/models/gadgets/Gadget.java
@@ -2,13 +2,13 @@ package models.gadgets;
 
 public abstract class Gadget {
 
-    private int id;
-    private String gadgetName;
-    private GadgetType gadgetType;
-    private float state;
-    private String valueTemplate;
-    private long pollDelaySec;
-    private boolean isPresent;
+    public int id;
+    public String gadgetName;
+    public GadgetType gadgetType;
+    public float state;
+    public String valueTemplate;
+    public long pollDelaySec;
+    public boolean isPresent;
 
     public Gadget(int id, String gadgetName, GadgetType gadgetType, float state, String valueTemplate, long pollDelaySec, boolean isPresent) {
         this.id = id;
@@ -17,7 +17,7 @@ public abstract class Gadget {
         this.state = state;
         this.valueTemplate = valueTemplate;
         this.pollDelaySec = pollDelaySec;
-        isPresent = false;
+        this.isPresent = isPresent;
     }
 
     public void setState(float state) {
